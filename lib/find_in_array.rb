008d92754be658a8752206572ee383ca711a1dfd
Tej Charfi <charfi@usc.edu>
2020-08-16 17:36:46 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,18 @@
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
+  ctr = 0
+
+  while ctr < array.length do
+   
+    if array[ctr] == value_to_find
+        return ctr
+    end   
+    ctr+=1
+  end
+  
+  
+  return nil
+  
+  
 end
