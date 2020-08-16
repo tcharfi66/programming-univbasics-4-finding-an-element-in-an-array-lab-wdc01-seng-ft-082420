f8882082b0afcce073b4a3f4a042f38595a838e7
Tej Charfi <charfi@usc.edu>
2020-08-16 17:30:45 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,11 +3,10 @@ def find_element_index(array, value_to_find)
   ctr = 0
 =
   while ctr < array.length do
-      if array[ctr] == value_to_find
+    if array[ctr] == value_to_find
         return ctr
-      end
+    end
     ctr+=1
-  end
   return nil
   
 end
